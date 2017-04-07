473e9d6f8c72258c7011a458306bfd412ec27d80
JJ Adonis <siikalawangjay@gmail.com>
2017-04-07 13:55:09 +0800

    Initial setup for ngRoute
    Setup ngRoute

--- app/js/app.js
+++ app/js/app.js
@@ -1,34 +1,19 @@
-'use strict';
+(function () {
 
-var eventsApp = angular.module('eventsApp', ['ngResource', 'ngRoute'])
-    .config(function($routeProvider, $locationProvider) {
-        $routeProvider.when('/newEvent',
-            {
-                templateUrl:'templates/NewEvent.html',
-                controller: 'EditEventController'
-            });
-        $routeProvider.when('/events',
-            {
-                templateUrl: 'templates/EventList.html',
-                controller: 'EventListController'
-            });
-        $routeProvider.when('/event/:eventId',
-            {
-                templateUrl: 'templates/EventDetails.html',
-                controller: 'EventController',
-                resolve: {
-                    event: function($route, eventData) {
-                        return eventData.getEvent($route.current.pathParams.eventId).$promise;
-                    }
-                }
-            });
-        $routeProvider.when('/sampleDirective',
-            {
-                templateUrl: 'templates/SampleDirective.html',
-                controller: 'SampleDirectiveController'
-            })
-        $routeProvider.otherwise({redirectTo: '/events'});
+    var app = angular.module('ngPokemonApp', ['ngResource', 'ngRoute'])
+        .config(function ($routeProvider, $locationProvider) {
 
-        $locationProvider.html5Mode(true);
+            $routeProvider.when('/', {
+                    templateUrl: 'templates/views/Pokedex.html',
+                    controller: 'PokedexController',
+                    controllerAs: 'pokedexCtrl'
+                })
+                .otherwise({
+                    redirectTo: '/'
+                });
 
-    });
+            $locationProvider.html5Mode(true).hashPrefix('!');
+
+        });
+
+}());
--- scripts/web-server.js
+++ scripts/web-server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var path = require('path');
-var events = require('./eventsController');
+//var events = require('./eventsController');
 var app = express();
 var rootPath = path.normalize(__dirname + '/../');
 var bodyParser = require('body-parser');
@@ -9,9 +9,9 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(express.static( rootPath + '/app'));
 
-app.get('/data/event/:id', events.get);
-app.get('/data/event', events.getAll);
-app.post('/data/event/:id', events.save);
+//app.get('/data/event/:id', events.get);
+//app.get('/data/event', events.getAll);
+//app.post('/data/event/:id', events.save);
 app.get('*', function(req, res) { res.sendFile(rootPath + '/app/index.html'); });
 
 app.listen(8000);
